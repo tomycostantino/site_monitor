765f3b0ca12bf6289f6be6096b08836792855bb8
Tomas Costantino <tomascostantino.au@gmail.com>
2025-07-02 04:18:11 +1000

    Basic site controller and views

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,44 @@
+class SitesController < ApplicationController
+  before_action :set_sites, only: [ :index ]
+  before_action :set_site, only: [ :show, :new, :edit, :destroy ]
+  def index
+    @sites = Site.all
+  end
+
+  def show
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      redirect_to @site, notice: "Site was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @site.destroy
+    redirect_to root_path
+  end
+
+  private
+  def set_sites
+    @sites = Site.all.order(:created_at)
+  end
+
+  def set_site
+    @site = params[:id] ? Site.find(params[:id]) : Site.new
+  end
+
+  def site_params
+    params.require(:site).permit(:url, :monitor_start_time, :monitor_end_time, :frequency_seconds, :timezone_offset_hours, :active)
+  end
+end
